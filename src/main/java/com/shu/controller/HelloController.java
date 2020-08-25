f5f829409e9d7bd4c0b927abc000e24e5dcc4b75
unknown <you@example.com>
2020-08-25 12:55:20 +0800

    mybatis generator

---
+++ src/main/java/com/shu/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.shu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author Tsenglying
+ * @Date 2020/8/24 17:01
+ * @Version 1.0
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping("/quick")
+    public String HelloTest(){
+        return"hello";
+    }
+}
